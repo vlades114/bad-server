6cee15049279c480a09af27468127eb8c05a0902
Vladislav Skulachenko <vladik-skulahenko@yandex.ru>
2026-09-08 16:52:28 +0300

    fix: валидация данных в авторизации (NoSQL-инъекция в login/register)

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -10,15 +10,19 @@ import {
 } from '../controllers/auth'
 import auth from '../middlewares/auth'
 import csrfProtection from '../middlewares/csrf'
+import {
+    validateAuthentication,
+    validateUserBody,
+} from '../middlewares/validations'
 
 const authRouter = Router()
 
 authRouter.get('/user', auth, getCurrentUser)
 authRouter.patch('/me', auth, csrfProtection, updateCurrentUser)
 authRouter.get('/user/roles', auth, getCurrentUserRoles)
-authRouter.post('/login', login)
+authRouter.post('/login', validateAuthentication, login)
 authRouter.get('/token', refreshAccessToken)
 authRouter.get('/logout', logout)
-authRouter.post('/register', register)
+authRouter.post('/register', validateUserBody, register)
 
 export default authRouter
