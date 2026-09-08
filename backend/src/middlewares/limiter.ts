b686232485dd45d28e675cde057251023a357276
Vladislav Skulachenko <vladik-skulahenko@yandex.ru>
2026-09-08 18:18:28 +0300

    fix: поднят лимит рейт-лимитера до 50 запросов в минуту

--- backend/src/middlewares/limiter.ts
+++ backend/src/middlewares/limiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 export const limiter = rateLimit({
     windowMs: 60 * 1000,
-    limit: 20,
+    limit: 50,
     standardHeaders: true,
     legacyHeaders: false,
 })
