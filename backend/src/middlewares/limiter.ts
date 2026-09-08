49550cd613a9bd81c5570f035a9dd72fe01ba0d9
Vladislav Skulachenko <vladik-skulahenko@yandex.ru>
2026-09-08 17:33:35 +0300

    fix: снижен лимит рейт-лимитера

--- backend/src/middlewares/limiter.ts
+++ backend/src/middlewares/limiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 export const limiter = rateLimit({
     windowMs: 60 * 1000,
-    limit: 100,
+    limit: 20,
     standardHeaders: true,
     legacyHeaders: false,
 })
